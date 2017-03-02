266dfdc25e25a2f7d1ed57eb055fd59414585b1a
Wende Mendes <wende@bluesoft.com.br>
2017-03-02 09:01:01 -0300

    ola mundo

--- src/main/java/com/controle/clientes/ControleClientesApplication.java
+++ src/main/java/com/controle/clientes/ControleClientesApplication.java
@@ -8,5 +8,7 @@ public class ControleClientesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleClientesApplication.class, args);
+		
+		System.out.println("Ola mundo");
 	}
 }
