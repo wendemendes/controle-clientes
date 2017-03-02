2f6ca45492fa7f967eaee1faff474702d4c0875c
Wende Mendes <wende@bluesoft.com.br>
2017-03-02 08:58:25 -0300

    inicio do projeto

---
+++ src/main/java/com/controle/clientes/ControleClientesApplication.java
@@ -0,0 +1,12 @@
+package com.controle.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleClientesApplication.class, args);
+	}
+}
